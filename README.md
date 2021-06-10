23c87d751757bae06b64000ffb1a7a18e33b79fd
krishivagrawal <79085044+krishivagrawal@users.noreply.github.com>
2021-06-10 11:35:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# -geologist-
+# Geologist
+Geologist Project
+https://krishivagrawal.github.io/-Geologist/
+
+https://simranvora.github.io/krishiv-s-geologist/
---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var stone, rubber,hammer;
+
+function setup(){
+
+       createCanvas(1200,600);
+
+
+ engine = Engine.create();
+ world = engine.world;
+
+ plane = new Plane(600,height,1200,20)
+ iron = new Iron(300,350);
+ stone = new Stone(700,320,100,100);
+
+ rubber=new Rubber(900,450,70);
+ hammer = new Hammer(10,100);
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+plane.display();
+hammer.display();
+stone.display();
+rubber.display();
+iron.display();
+
+    
+ 
+}
